80010d5a51d0674672b8ce9a74f1564e3f7774c6
Sean Leather <sean.leather@gmail.com>
2018-10-16 10:08:31 +0200

    Build without using Jakefile.js

--- Formula/madoko.rb
+++ Formula/madoko.rb
@@ -10,13 +10,14 @@ class Madoko < Formula
   depends_on "node"
   depends_on "spl/koka/koka@0.6.0-dev" => :build
 
-  # Fix build to use brew-installed koka-0.6.0-dev.
-  patch do
-    url "https://raw.githubusercontent.com/spl/homebrew-koka/master/patch/madoko-1.1.6-jakefile.patch"
-    sha256 "a8b1816c9daabc45457c6dcf25e0f8301e9aa9e4968d0b5c9268980712c8463d"
-  end
-
   def install
+    # Build Madoko with Koka and put files into lib/
+    system "koka-0.6.0-dev", "-isrc", "-c", "-olib", "--outname=madoko", "main"
+
+    # Move other files into lib/
+    mv Dir["src/cli.js", "contrib/*/*.js"], "lib"
+
+    # Install with npm
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
