cb972ededfc3d68f2297fadf9cc750b4c806cfe7
Sean Leather <sean.leather@gmail.com>
2018-10-16 10:12:23 +0200

    Update madoko desc and homepage

--- Formula/madoko.rb
+++ Formula/madoko.rb
@@ -1,8 +1,8 @@
 require "language/node"
 
 class Madoko < Formula
-  desc "..."
-  homepage "..."
+  desc "Markdown processor for academic and technical articles"
+  homepage "https://www.madoko.net/"
   url "https://github.com/koka-lang/madoko.git",
     :revision => "5cee03a0e38177792635153dccc7616e34ed475b"
   version "1.1.6"
